b64b0ee3aca817b76f8675166c3ede47aca9bf14
mrtsnt <martynas.seniut@gmail.com>
2022-07-21 19:04:56 +0300

    refactor to use instruction struct

--- main.go
+++ main.go
@@ -1,9 +1,25 @@
 package main
 
 import (
-	"os"
 	"fmt"
+	"log"
+	"os"
 )
+
+type instruction struct {
+	nibbles []uint8
+	jumpPosition uint16
+	value uint8
+}
+
+func readInstruction(bts []byte) instruction {
+	u16 := uint16(bts[0]) << 8 | uint16(bts[1])
+	return instruction{
+		nibbles: []uint8{ bts[0] >> 4, bts[0] & 0x0F, bts[1] >> 4, bts[1] & 0x0F },
+		jumpPosition: u16 & 0x0FFF,
+		value: uint8(u16 & 0x00FF),
+	}
+}
 
 func main() {
 	if len(os.Args) != 2 {
@@ -15,32 +31,40 @@ func main() {
 	handle := newSdl()
 	defer handle.cleanup()
 
+	runEmulator(chip, handle)
+}
+
+func runEmulator(chip *chip8, handle *sdlHandle) {
 	for {
-		instruction := uint16(chip.memory[chip.pc]) << 8 | uint16(chip.memory[chip.pc + 1])
-		firstByte := instruction >> 12
+		instr := readInstruction(chip.memory[chip.pc : chip.pc+2])
 		chip.pc += 2
 
-		switch firstByte {
+		switch instr.nibbles[0] {
 		case 0x0:
 			chip.clearScreen()
 			handle.drawWindow(chip)
+
 		case 0x1:
-			chip.pc = instruction & 0x0FFF
+			chip.pc = instr.jumpPosition
+
 		case 0x6:
-			register := (instruction & 0x0F00) >> 8
-			chip.registers[register] = uint8(instruction & 0x00FF)
+			register := instr.nibbles[1]
+			chip.registers[register] = instr.value
+
 		case 0x7:
-			register := (instruction & 0x0F00) >> 8
-			chip.registers[register] += uint8(instruction & 0x00FF)
+			register := instr.nibbles[1]
+			chip.registers[register] += instr.value
+
 		case 0xA:
-			chip.i = instruction & 0x0FFF
+			chip.i = instr.jumpPosition
+
 		case 0xD:
 			chip.registers[0xF] = 0
-			rows := instruction & 0x000F
-			y := chip.registers[(instruction & 0x00FF) >> 4] % 32
-			for r := uint16(0); r < rows && y < 32; r++ {
-				x := chip.registers[(instruction >> 8) & 0x0F] % 64
-				sprite := chip.memory[chip.i + r]
+			rows := instr.nibbles[3]
+			y := chip.registers[instr.nibbles[2]] % 32
+			for r := uint8(0); r < rows && y < 32; r++ {
+				x := chip.registers[instr.nibbles[1]] % 64
+				sprite := chip.memory[chip.i+uint16(r)]
 				for bytePos := uint8(0); bytePos < 8 && x < 64; bytePos++ {
 					bitSet := sprite & (1 << (7 - bytePos))
 					if bitSet > 0 && chip.screen[y][x] {
@@ -54,8 +78,9 @@ func main() {
 				y++
 			}
 			handle.drawWindow(chip)
+
 		default:
-			panic("unknown instruction")
+			log.Fatal("unknown instruction", instr)
 		}
 	}
 }
